d4ef588bc016185fc21441172c69a459aa32683f
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-05 19:56:29 +0100

    Create link.php

---
+++ link.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+$stock = $_SESSION["valeurst"];
+$nom = $_POST["nom"];
+$prenom = $_POST["prenom"];
+
+
+?>
+
+<html>
+
+<head>
+
+    <title>recuperation</title>
+</head>
+
+<body>
+    <table>
+        <tr>
+            <td>La valeur stocké est:</td>
+            <td><?php echo $stock; ?></td>
+        </tr>
+        <tr>
+            <td>Le nom est:</td>
+            <td><?php print $nom; ?></td>
+        </tr>
+        <tr>
+            <td>Le prenom est:</td>
+            <td><?= $prenom ?></td>
+
+
+        </tr>
+    </table>
+
+
+</body>
+
+
+</html>
